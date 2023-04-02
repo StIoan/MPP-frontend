620a4629a03464e85ea3b0d7ef63e9d32c97a0dc
Stoicovici Ioan <stoicoviciioan@yahoo.com>
2023-04-02 18:35:02 +0300

    /

--- src/pages/Car.js
+++ src/pages/Car.js
@@ -7,7 +7,7 @@ export default function Country() {
         loadCars()
     },[])
     const loadCars=async() => {
-        const result=await axios.get("http://localhost:8080/cars")
+        const result=await axios.get(`http://localhost:80/cars`)
         console.log(result.data)
         setCars(result.data)
     }
