15deaebe3b2832df7592090c4f063450a69eeafa
Stoicovici Ioan <stoicoviciioan@yahoo.com>
2023-04-03 11:17:38 +0300

    /

--- src/owner/SortOwner.js
+++ src/owner/SortOwner.js
@@ -9,7 +9,7 @@ export default function SortOwner() {
     const loadUsers=async() => {
         const result=await axios.get(`http:localhost:80/owners`)
         setUsers(result.data)
-        users.map().sort((a, b) => a.height - b.height)
+        setUsers(users.map().sort((a, b) => a.height - b.height))
     }
 
     return (
