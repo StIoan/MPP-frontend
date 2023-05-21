ca554418e58fcda7eb0df0781b9391288f4dbff5
Stoicovici Ioan <stoicoviciioan@yahoo.com>
2023-05-21 18:32:56 +0300

    ,

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar() {
         <nav className="navbar navbar-expand-md navbar-dark bg-dark">
         <div className="container-fluid">
             <div class="collapse navbar-collapse" id="navbarNav">
-              <ul class="navbar-nav ms-auto">
+              <ul class="navbar-nav mx-auto">
                 <li class="nav-item">
                   <a class="nav-link active" href="/">User</a>
                 </li>
